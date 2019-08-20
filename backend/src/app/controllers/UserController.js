71537c1586b898e35aac80ead76c4778b1384af6
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-20 15:30:38 -0300

    Update do usuário

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -25,7 +25,30 @@ class UserController {
 
   async update(req, res) {
     try {
-      return res.json({ ok: true });
+      const { email, oldPassword } = req.body;
+
+      const user = await User.findByPk(req.userId);
+
+      if (email !== user.email) {
+        const userExists = await User.findOne({
+          where: { email },
+        });
+
+        if (userExists)
+          return res.status(400).json({ error: 'Email already exists.' });
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword)))
+        return res.status(401).json({ error: 'Password does not match.' });
+
+      const { id, name, provider } = await user.update(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
     } catch (error) {
       return res.status(400).json(error);
     }
