5de085c0c4f1a2f22a08584b1087a9c5ee620fff
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-20 12:02:24 -0300

    ESLint, Prettier & EditorConfig

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "salv" })
-});
+routes.get('/', (req, res) => res.json({ message: 'salv' }));
 
 export default routes;
