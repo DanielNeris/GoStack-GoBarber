cadf6801e1e8b14cbef74c262f53f9749979dc36
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-20 14:28:44 -0300

    Cadastro de usuários

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (userExists)
+        return res.status(400).json({ error: 'Email already exists.' });
+
+      const { id, name, email, provider } = await User.create(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
+}
+
+export default new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Daniel',
-    email: 'daniel@daniel2.com',
-    password_hash: '1323121321',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
