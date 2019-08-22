3b3d91fa7f6064b37b474e02551ba40d411d27c4
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-22 09:37:24 -0300

    Listando agenda do prestador

--- backend/src/app/controllers/ScheduleController.js
+++ backend/src/app/controllers/ScheduleController.js
@@ -1,3 +1,6 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -11,7 +14,21 @@ class ScheduleController {
       if (!checkUserProvider)
         return res.status(400).json({ error: 'User is not a provider' });
 
-      return res.json({ ok: true });
+      const { date } = req.query;
+      const parsedDate = parseISO(date);
+
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: req.userId,
+          canceled_at: null,
+          date: {
+            [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+          },
+        },
+        order: ['date'],
+      });
+
+      return res.json(appointments);
     } catch (error) {
       return res.status(400).json(error);
     }
