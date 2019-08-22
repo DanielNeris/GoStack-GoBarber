cd551a1f8601ed8a3d463e6e549cd84f2974976b
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-22 11:53:17 -0300

    Marcar notificações como lidas

--- backend/src/app/controllers/NotificationController.js
+++ backend/src/app/controllers/NotificationController.js
@@ -28,6 +28,20 @@ class NotificationController {
       return res.status(400).json(error);
     }
   }
+
+  async update(req, res) {
+    try {
+      const notification = await Notification.findByIdAndUpdate(
+        req.params.id,
+        { read: true },
+        { new: true }
+      );
+
+      return res.json(notification);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
 }
 
 export default new NotificationController();
