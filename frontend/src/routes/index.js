de811e6d5971a9836e21ae4081049453196ebaed
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-12 17:00:49 -0300

    Utilizando Root Import

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SingIn from '../pages/SingIn';
-import SingUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import SingIn from '~/pages/SingIn';
+import SingUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
