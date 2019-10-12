8de749c037e64b0d81c99f08eae76c8e0e330bcc
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-12 16:12:55 -0300

    Layouts por página

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTyes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivade,
@@ -17,10 +20,16 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
   return (
     <Route
       {...rest}
-      component={Component}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
     />
   );
 }
