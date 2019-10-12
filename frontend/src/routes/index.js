84c1f3b38c080d86e70ae22cdfd12991ccf612d0
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-12 14:56:39 -0300

    Configurando rotas

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SingIn from '../pages/SingIn';
+import SingUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SingIn} />
+      <Route path="/register" component={SingUp} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
