20560fed05ce5b621faba317aa10f59dd55fb6a5
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-13 15:50:00 -0300

    Logout da aplicação

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
-// import { signOut } from '~/store/modules/auth/actions';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -18,7 +18,7 @@ export default function Profile() {
   }
 
   function handleSignOut() {
-    // dispatch(signOut());
+    dispatch(signOut());
   }
 
   return (
