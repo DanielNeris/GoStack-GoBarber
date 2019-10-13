f18f70c09d6e61e8ee9726bcd7f9c2b3bd6f148c
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-13 14:08:17 -0300

    Atualizando perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
   function handleSubmit(data) {
-
+    dispatch(updateProfileRequest(data));
   }
 
   return (
