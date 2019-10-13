f32abf18e250c1475508ca18d40f259566ada566
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-13 16:02:11 -0300

    Estilização do Dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
 
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>31 de hoje</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time available>
+          <strong>@4de agora</strong>
+          <span>Da de eu</span>
+        </Time>
+        <Time>
+          <strong>@4de agora</strong>
+          <span>Da de eu</span>
+        </Time>
+        <Time past>
+          <strong>@4de agora</strong>
+          <span>Da de eu</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
