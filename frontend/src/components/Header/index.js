cb27c8da5273a4d6f174558508b1a6b9baabe5e9
Daniel de Souza Neri <danielneris01@gmail.com>
2019-10-13 12:51:21 -0300

    Configurando Header

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import logo from '~/assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarbar" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Daniel Neris</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Daniel"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- frontend/src/pages/_layouts/default/index.js
+++ frontend/src/pages/_layouts/default/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from '~/components/Header';
+
 import { Wrapper } from './styles';
 
 export default function DefaultLayout({ children }) {
   return (
     <Wrapper>
+      <Header />
       {children}
     </Wrapper>
   );
